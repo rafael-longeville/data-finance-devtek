702d467b1c9aaea6cb2d23492a81db219b6adcdb
Rafael Longeville <rafael.longeville.pro@gmail.com>
2022-10-14 09:42:38 +0200

    modify typed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
           </p>
           <Typed
             className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-            strings={["BTB", "BTC", "SASS"]}
+            strings={["B2B", "B2C", "SASS"]}
             typeSpeed={120}
             backSpeed={140}
             loop
